41b1e627bedae971dcd036991bad7b073a17d092
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-02-06 20:11:24 +0000

    Increase throttle rate limits

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:300,1',
             'bindings',
         ],
     ];
@@ -61,7 +61,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'client' => CheckClientCredentials::class
     ];
 
     /**
